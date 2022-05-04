b04a4909b6a306d2913d5d74d7e8e96257b8b289
juanox44 <76264542+juanox44@users.noreply.github.com>
2022-05-04 18:58:52 -0400

    chore: modified "hello, world!" to "Hola mundo!"

--- petclub/views.py
+++ petclub/views.py
@@ -7,4 +7,4 @@ from rest_framework.views import APIView
 class HelloWorld(APIView):
     def get(self, request): # verbo de la peticion como un metodo
         # logica asociada al endpoint
-        return Response(data="Hello, World !", status=200) # respuesta del servicio
+        return Response(data="Hola mundo!", status=200) # respuesta del servicio
